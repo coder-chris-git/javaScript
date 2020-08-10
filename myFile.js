f8d8c7a777db754ac912af3f37aef835fa21b97b
Chris Davis <cdav49719@gmail.com>
2020-08-09 20:49:30 -0400

    Created useful functions folder

--- myFile.js
+++ myFile.js
@@ -1,13 +1,23 @@
-let myElement = document.createElement('h1')
+
+
+let myElement = document.createElement('h2')
+let mySubtitle = document.createElement('span')
 let mySpan = document.createElement('span')
-mySpan.innerText = 'Creating NavBar Element with JavaScript'
+mySpan.innerText = 'NavBar by Coder-Chris'
+
+mySpan.appendChild(mySubtitle)
+mySubtitle.innerText = ' written in JavaScript'
 myElement.appendChild(mySpan)
+
 
 let unOrderdList = document.createElement('ul')
 let listItemZero = document.createElement('li')
 let listItemOne = document.createElement('li')
 let listItemTwo = document.createElement('li')
 let listItemThree = document.createElement('li')
+
+
+
 
 listItemZero.innerText = 'Home'
 listItemOne.innerText = 'Catalog'
@@ -20,11 +30,12 @@ unOrderdList.appendChild(listItemThree)
 unOrderdList.appendChild(listItemOne)
 
 unOrderdList.setAttribute("style", 'display:flex ; justify-content: space-around; margin:0 auto ;color:whitesmoke;list-style-type:none;background-color:cornflowerBlue;height:100px; align-items:center ;font-weight:700;font-size:20px ; border-radius : 4px')
-
+mySubtitle.setAttribute("style", "padding:0;margin:0 ; font-size:18px;color:red")
 
 
 document.body.appendChild(myElement)
 document.body.appendChild(unOrderdList)
+document.body.appendChild(button)
 
 document.body.setAttribute("style", 'marign:0 auto; padding:0')
 
---
+++ usefulFunctions.js/math.js
@@ -0,0 +1,9 @@
+function sum(array) {
+    return array.reduce((a, b) => a + b, 0)
+}
+
+array1 = [3, 5]
+console.log(sum(array1))
+
+
+
